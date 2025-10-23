2fef9d2f3baee231ab5682f94518fe7a2d7fdcf4
SamcliFeedback <samclifeedback@gmail.com>
2025-10-23 14:03:15 +0800

    clean up

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,7 +5,6 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Smooth scroll to top on every route change
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [pathname]);
 
