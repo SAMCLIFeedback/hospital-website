b79471e2a4e2c11cb6e76536706414339b2a80d0
NEWNAME <NEWEMAIL>
2025-12-07 20:17:57 +0800

    created new model for patient token to be use for the patient feedbacks

---
+++ backend/models/PatientToken.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const PatientTokenSchema = new mongoose.Schema({
+  token: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  used: { 
+    type: Boolean, 
+    default: false 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  usedAt: { 
+    type: Date, 
+    default: null 
+  },
+  feedbackId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    default: null 
+  }
+}, { versionKey: false });
+
+module.exports = mongoose.model('PatientToken', PatientTokenSchema);
